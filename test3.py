f801e8e61a994ecb34bb6195597c8b4f8e0c7c51
Eduardo <101486227+HttpEduardo@users.noreply.github.com>
2023-08-03 01:55:19 -0300

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,22 @@
+def test_user_can_make_a_purchase():
+    # 1. Faça login no sistema.
+    # 2. Adicione um item ao carrinho.
+    # 3. Vá para a página de checkout.
+    # 4. Insira as informações de envio e pagamento.
+    # 5. Clique no botão Fazer o pedido.
+    # 6. Verifique se o pedido foi feito com sucesso.
+
+def test_user_can_generate_a_report():
+    # 1. Faça login no sistema.
+    # 2. Vá para a página de relatórios.
+    # 3. Selecione o relatório que deseja gerar.
+    # 4. Clique no botão Gerar relatório.
+    # 5. Verifique se o relatório foi gerado com sucesso.
+
+def test_user_can_manage_inventory():
+    # 1. Faça login no sistema.
+    # 2. Vá para a página de estoque.
+    # 3. Adicione um novo item ao estoque.
+    # 4. Edite o estoque existente.
+    # 5. Remova um item do estoque.
+    # 6. Verifique se o estoque foi gerenciado com sucesso.
