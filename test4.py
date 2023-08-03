b329b663d84d1bf4e29912807508cdc7632a91a3
Eduardo <101486227+HttpEduardo@users.noreply.github.com>
2023-08-03 01:55:32 -0300

    Create test4.py

---
+++ test4.py
@@ -0,0 +1,9 @@
+def test_login_form_communicates_with_database():
+    # 1. Preencha o formulário de login com credenciais válidas.
+    # 2. Clique no botão Enviar.
+    # 3. Verifique se o usuário é autenticado com sucesso.
+
+def test_login_form_communicates_with_database_with_invalid_credentials():
+    # 1. Preencha o formulário de login com credenciais inválidas.
+    # 2. Clique no botão Enviar.
+    # 3. Verifique se o usuário não é autenticado.
